721f83e0ddb866e4c3eca4a5eb46a0a337eac53b
Sohaib Ali <sohaibali1913@gmail.com>
2024-01-11 16:43:40 +0500

    changes of 11-01

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./dist/index.html", "./src/input.js"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+
+      }
+    },
   },
   plugins: [],
 }
 
+
+
+
+// shadow-[.4rem_.4rem_1rem_#333,-.4rem_-.4rem_1rem_#ccc] hover:shadow-inner[.4rem_.4rem_1rem_#333,-.4rem_-.4rem_1rem_#ccc]
